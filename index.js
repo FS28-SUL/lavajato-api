e12244b2b82129d8606241ccc0ca1b6c209a3f29
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-12-01 12:03:45 -0300

    updates

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cargoRoutes from "./src/routes/cargoRoutes.js"
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Seja bem-vindo");
+});
+
+//middleware
+app.use('/cargos', cargoRoutes);
+
+
+
+
+
+app.use((req, res) => {
+    res.status(404).json({
+        mensagem: "pagina não encontrada"
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Serviço on: http://localhost:3000");
+});
